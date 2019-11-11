0683d0e662850283e0cc35196da481bb4b90d13b
Pranav Nutalapati <pranavnutalapati@gmail.com>
2019-11-11 11:34:00 +0530

    Addon to previous commit

--- routes/teachers.js
+++ routes/teachers.js
@@ -3,6 +3,9 @@ const controller = require('../controllers/api/teachers')
 const express = require('express')
 const router = express.Router()
 
+// -- DEBUG ROUTES --
 router.post('/', controller.createTeacher)
+router.get('/', contorller.getTeachers)
+// -- END OF DEBUG ROUTES --
 
 module.exports = router
